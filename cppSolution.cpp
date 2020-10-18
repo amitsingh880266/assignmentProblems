9aad63397f82e930bb9cedceb9fd792730d2ee73
Amit <amitsingh880266@gmail.com>
2020-10-19 01:47:01 +0530

    Final solution

--- cppSolution.cpp
+++ cppSolution.cpp
@@ -1,37 +1,27 @@
 #include<iostream>
+#include <bits/stdc++.h> 
 using namespace std;
-int matchingPair(int color[],int n,int freq[]){
-    for(int i=0;i<n;i++){
-        int count =1;
-        for(int j=i+1;j<n;j++){
-            if(color[i]==color[j]){
-                count++;
-                freq[j]=0;
-            }
-            if(freq[i]!=0){
-                freq[i]=count;
+int matchingPair(vector<int> color,int n){
+        int pair =0;                    // pair variable initialized with zero
+        sort(color.begin(),color.end()); // sort color array
+        for(int i=0;i<n;i++){           // loop through 0 to n-1 in color array
+            if(color[i]==color[i+1]){   // if color[i] is equal to color[i+1] then skip one step by i++
+                i++;            
+                pair++;                  // increment pair by one as two element found same   
             }
         }
-    }
-    int total=0;
-
-    for(int i=0;i<n;i++){
-        total+=freq[i]/2;
-    }
-    return total;
+    return pair;                          // return the total number of pair to main function 
 }
 int main(){
-    // Declare n as the size of the color array
     int n;
-    int * color;                //Declare color array
-    int *freq;                  //Declare frequency array
+    vector<int> color;
+    int temp;
     cin>>n;
-    color = new int[n];         // assinging memory dynamically of size n
-    freq= new int[n];           // assinging memory dynamically of size n
+
     for(int i=0;i<n;i++){
-        cin>>color[i];
-        freq[i]=-1;             //Initially assinging freq of each color as -1
+        cin>>temp;
+        color.push_back(temp);
     }
-    cout<<matchingPair(color,n,freq);  //Calling and printing the value of total pairs returned by function ->matchingPair
+    cout<<matchingPair(color,n);
     return 0;
 }
